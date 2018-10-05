b5a5241a22f61504f8b2215e263a1ed55a55db46
Max <monnoroch@gmail.com>
2018-10-06 00:22:42 +0100

    Move autoinject to a separate package. (#20)
    This is possible because it's implemented with dynamic providers modules now.
    
    Also cover it with tests and auto injecting support pointers to structs.
    
    Related to #17.

--- auto/autoinject.go
+++ auto/autoinject.go
@@ -1,59 +1,75 @@
-package inject
+package autoinject
 
 import (
 	"fmt"
 	"reflect"
+
+	"github.com/monnoroch/go-inject"
 )
 
 /// Default annotation for auto-injected types.
+/// Can be used if the program does not have two components of the same type.
 type Auto struct{}
 
 /// An interface to be implemented to support auto-injecting a type.
 type AutoInjectable interface {
 	/// Returns a mapping of field names to annotations.
-	/// Omitted fields imply `inject.Auto` annotation.
-	/// Not implementing this method implies all fields having `inject.Auto` annotation.
+	/// Omitted fields imply `autoinject.Auto` annotation.
+	/// Not implementing this method implies all fields having `autoinject.Auto` annotation.
 	ProvideAutoInjectAnnotations() interface{}
 }
 
 type autoInjectModule struct {
 	typePointer interface{}
-	annotation  Annotation
+	annotation  inject.Annotation
 	annotations interface{}
 	cached      bool
 }
 
-/// Create a module for providing a struct type with an annotation with dependencies
-/// inferred from the annotations struct.
-func AutoInjectModule(typePointer interface{}, annotation Annotation, annotations interface{}) Module {
+/// Create a module for automatically providing a struct type with the default `autoinject.Auto` annotation.
+func AutoInjectModule(typePointer interface{}) autoInjectModule {
 	return autoInjectModule{
 		typePointer: typePointer,
-		annotation:  annotation,
-		annotations: annotations,
+		annotation:  Auto{},
+		annotations: struct{}{},
 		cached:      false,
 	}
 }
 
-/// Create a module for providing a struct type with an annotation with dependencies
-/// inferred from the annotations struct.
-func AutoInjectCachedModule(typePointer interface{}, annotation Annotation, annotations interface{}) Module {
-	return autoInjectModule{
-		typePointer: typePointer,
-		annotation:  annotation,
-		annotations: annotations,
-		cached:      true,
-	}
+/// Auto-inject the value with a custom annotation.
+func (self autoInjectModule) WithAnnotation(annotation inject.Annotation) autoInjectModule {
+	self.annotation = annotation
+	return self
+}
+
+/// Auto-inject the value with a custom annotation.
+func (self autoInjectModule) WithAnnotations(annotations interface{}) autoInjectModule {
+	self.annotations = annotations
+	return self
+}
+
+/// Make the generated provider cached.
+func (self autoInjectModule) Cached() autoInjectModule {
+	self.cached = true
+	return self
+}
+
+/// Make the generated provider not cached.
+func (self autoInjectModule) NotCached() autoInjectModule {
+	self.cached = false
+	return self
 }
 
 var autoAnnotationType = reflect.TypeOf(Auto{})
 var autoInjectableType = reflect.TypeOf((*AutoInjectable)(nil)).Elem()
 
-func (self autoInjectModule) Providers() ([]Provider, error) {
+func (self autoInjectModule) Providers() ([]inject.Provider, error) {
 	valueType := reflect.TypeOf(self.typePointer).Elem()
 	annotationType := reflect.TypeOf(self.annotation)
+	dereferencedValueType, derefed := getDereferencedType(valueType)
 
-	if valueType.Kind() != reflect.Struct {
-		return nil, fmt.Errorf("%v is not a struct", reflect.TypeOf(self.typePointer))
+	if dereferencedValueType.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("%v is not a struct", dereferencedValueType)
 	}
 
 	annotationByField := map[string]reflect.Type{}
@@ -65,15 +81,15 @@ func (self autoInjectModule) Providers() ([]Provider, error) {
 	extractAnnotations(self.annotations, annotationByField)
 
 	providerArgumentTypes := []reflect.Type{}
-	for i := 0; i < valueType.NumField(); i += 1 {
-		field := valueType.Field(i)
+	for i := 0; i < dereferencedValueType.NumField(); i += 1 {
+		field := dereferencedValueType.Field(i)
 		fieldAnnotationType, ok := annotationByField[field.Name]
 		if !ok {
 			fieldAnnotationType = autoAnnotationType
 		}
 		providerArgumentTypes = append(providerArgumentTypes, field.Type, fieldAnnotationType)
 	}
-	provider := Provider{
+	provider := inject.Provider{
 		Function: reflect.MakeFunc(
 			reflect.FuncOf(
 				providerArgumentTypes,
@@ -81,16 +97,26 @@ func (self autoInjectModule) Providers() ([]Provider, error) {
 				false,
 			),
 			func(arguments []reflect.Value) []reflect.Value {
-				result := reflect.New(valueType).Elem()
+				result := reflect.New(dereferencedValueType).Elem()
 				for i := 0; i < result.NumField()*2; i += 2 {
 					result.Field(i / 2).Set(arguments[i])
+				}
+				if derefed {
+					result = result.Addr()
 				}
 				return []reflect.Value{result, reflect.Zero(annotationType)}
 			},
 		),
 		Cached: self.cached,
 	}
-	return []Provider{provider}, nil
+	return []inject.Provider{provider}, nil
+}
+
+func getDereferencedType(valueType reflect.Type) (reflect.Type, bool) {
+	if valueType.Kind() == reflect.Ptr {
+		return valueType.Elem(), true
+	}
+	return valueType, false
 }
 
 func extractAnnotations(annotationsStruct interface{}, annotationByField map[string]reflect.Type) {
---
+++ auto/autoinject_test.go
@@ -0,0 +1,199 @@
+package autoinject
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/monnoroch/go-inject"
+)
+
+type Annotation struct{}
+
+type AutoInjectTests struct {
+	suite.Suite
+}
+
+func (self *AutoInjectTests) TestEmptyStruct() {
+	type Struct struct{}
+
+	provider := self.getProvider(AutoInjectModule(new(Struct)))
+	self.False(provider.Cached)
+
+	value, annotation := self.call(provider, []reflect.Value{})
+
+	self.Equal(Struct{}, value)
+
+	_, ok := annotation.(Auto)
+	self.True(ok)
+}
+
+func (self *AutoInjectTests) TestStructWithFields() {
+	testValue := 10
+
+	type Struct struct {
+		Value int
+	}
+
+	provider := self.getProvider(AutoInjectModule(new(Struct)))
+	self.False(provider.Cached)
+
+	value, annotation := self.call(provider, []reflect.Value{
+		reflect.ValueOf(int(testValue)), reflect.ValueOf(Auto{}),
+	})
+
+	self.Equal(Struct{
+		Value: testValue,
+	}, value)
+
+	_, ok := annotation.(Auto)
+	self.True(ok)
+}
+
+func (self *AutoInjectTests) TestPointerToEmptyStruct() {
+	type Struct struct{}
+
+	provider := self.getProvider(AutoInjectModule(new(*Struct)))
+	value, _ := self.call(provider, []reflect.Value{})
+	self.Equal(&Struct{}, value)
+}
+
+func (self *AutoInjectTests) TestPointerToStructWithFields() {
+	testValue := 10
+
+	type Struct struct {
+		Value int
+	}
+
+	provider := self.getProvider(AutoInjectModule(new(*Struct)))
+	value, _ := self.call(provider, []reflect.Value{
+		reflect.ValueOf(int(testValue)), reflect.ValueOf(Auto{}),
+	})
+
+	self.Equal(&Struct{
+		Value: testValue,
+	}, value)
+}
+
+func (self *AutoInjectTests) TestCustomAnnotation() {
+	type Struct struct{}
+
+	provider := self.getProvider(AutoInjectModule(new(Struct)).WithAnnotation(Annotation{}))
+	_, annotation := self.call(provider, []reflect.Value{})
+	_, ok := annotation.(Annotation)
+	self.True(ok)
+}
+
+func (self *AutoInjectTests) TestCustomAnnotations() {
+	testValue := 10
+	type Struct struct {
+		Value int
+	}
+
+	provider := self.getProvider(AutoInjectModule(new(Struct)).WithAnnotations(struct {
+		Value Annotation
+	}{}))
+	value, annotation := self.call(provider, []reflect.Value{
+		reflect.ValueOf(int(testValue)), reflect.ValueOf(Annotation{}),
+	})
+
+	self.Equal(Struct{
+		Value: testValue,
+	}, value)
+
+	_, ok := annotation.(Auto)
+	self.True(ok)
+}
+
+type AutoInjectableStruct struct {
+	Value int
+}
+
+func (self AutoInjectableStruct) ProvideAutoInjectAnnotations() interface{} {
+	return struct{}{}
+}
+
+func (self *AutoInjectTests) TestAutoInjectable() {
+	testValue := 10
+
+	provider := self.getProvider(AutoInjectModule(new(AutoInjectableStruct)))
+	value, annotation := self.call(provider, []reflect.Value{
+		reflect.ValueOf(int(testValue)), reflect.ValueOf(Auto{}),
+	})
+
+	self.Equal(AutoInjectableStruct{
+		Value: testValue,
+	}, value)
+
+	_, ok := annotation.(Auto)
+	self.True(ok)
+}
+
+type CustomAutoInjectableStruct struct {
+	Value int
+}
+
+func (self CustomAutoInjectableStruct) ProvideAutoInjectAnnotations() interface{} {
+	return struct {
+		Value Annotation
+	}{}
+}
+
+func (self *AutoInjectTests) TestCustomAutoInjectable() {
+	testValue := 10
+
+	provider := self.getProvider(AutoInjectModule(new(CustomAutoInjectableStruct)))
+	value, annotation := self.call(provider, []reflect.Value{
+		reflect.ValueOf(int(testValue)), reflect.ValueOf(Annotation{}),
+	})
+
+	self.Equal(CustomAutoInjectableStruct{
+		Value: testValue,
+	}, value)
+
+	_, ok := annotation.(Auto)
+	self.True(ok)
+}
+
+func (self *AutoInjectTests) TestCached() {
+	provider := self.getProvider(AutoInjectModule(new(struct{})).Cached())
+	self.True(provider.Cached)
+}
+
+func (self *AutoInjectTests) TestNotCached() {
+	provider := self.getProvider(AutoInjectModule(new(struct{})).Cached().NotCached())
+	self.False(provider.Cached)
+}
+
+func (self *AutoInjectTests) TestPrimitive() {
+	_, err := AutoInjectModule(new(int)).Providers()
+	self.Contains(err.Error(), "int is not a struct")
+}
+
+func (self *AutoInjectTests) TestDoublePointerToStruct() {
+	type Struct struct{}
+	_, err := AutoInjectModule(new(**Struct)).Providers()
+	self.Contains(err.Error(), "*autoinject.Struct is not a struct")
+}
+
+func (self *AutoInjectTests) getProvider(module inject.DynamicModule) inject.Provider {
+	providers, err := module.Providers()
+	self.Require().Nil(err)
+	self.Equal(1, len(providers))
+	return providers[0]
+}
+
+func (self *AutoInjectTests) call(provider inject.Provider, arguments []reflect.Value) (interface{}, interface{}) {
+	function, ok := provider.Function.(reflect.Value)
+	self.True(ok)
+
+	outputs := function.Call(arguments)
+	self.Equal(2, len(outputs))
+
+	return outputs[0].Interface(), outputs[1].Interface()
+}
+
+func TestAutoInject(t *testing.T) {
+	suite.Run(t, new(AutoInjectTests))
+}
